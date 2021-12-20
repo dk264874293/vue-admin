8f9198c29612152eb58cb2e831e3a4fc72041b68
汪培良 <pl.wang@medcloud.cn>
2021-12-20 14:51:21 +0800

    feat:  初始化项目路由

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    <router-link to="/">首页</router-link> |
+    <router-link to="/about">关注我们</router-link>
+  </div>
+
+  <router-view></router-view>
 </template>
 
 <style>
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts"></script>
+
+<template>
+  <div>首页</div>
+</template>
+
+<style></style>
